c4ecd43a6889d0c759abee08a77c145f8f349862
Matt Fraser <liquidpt@outlook.com>
2021-11-24 11:42:31 -0800

    add implicit usings

--- src/AmVinDecoder/Program.cs
+++ src/AmVinDecoder/Program.cs
@@ -3,8 +3,6 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
-using System.IO;
 using AmVinDecoder.Properties;
 using AmVinDecoderLib;
 using Microsoft.Extensions.Configuration;
--- src/AmVinDecoderLib/Repositories/BaseRepository.cs
+++ src/AmVinDecoderLib/Repositories/BaseRepository.cs
@@ -3,10 +3,6 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using AmVinDecoderLib.VinComponents;
 using AmVinDecoderLib.VinComponents.Enums;
 using EnsureThat;
